c0cb1ca898e7ad9daba41eacc583279b8729d3a2
rkaufusi <rhettkaufusi@gmail.com>
2022-04-04 21:55:46 -0700

    changed grid item xs and md, image to avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import Grid from '@mui/material/Grid'
 import './App.css';
-
+//import {makeStyles} from '@mui/styles'
+import Avatar from '@mui/material/Avatar';
 
 function App() {
+
   return (
     <div className="App">
       <Grid container>
-        <Grid item>
+        <Grid item xs={12} md={6}>
             Rhett Kaufusi
         </Grid>
-        <Grid item>
-          <img src="https://avatars.githubusercontent.com/u/29996077?s=400&u=7ff0609f49dde0b01183b37e8d6384a9ef17f540&v=4" alt="Picture of Rhett"/>
+        <Grid item xs={12} md={6}>
+          <Avatar 
+            src="https://avatars.githubusercontent.com/u/29996077?s=400&u=7ff0609f49dde0b01183b37e8d6384a9ef17f540&v=4" 
+            alt="Picture of Rhett" 
+            sx={{ width: 480, height: 480 }}
+          />
         </Grid>
       </Grid>
     </div>
